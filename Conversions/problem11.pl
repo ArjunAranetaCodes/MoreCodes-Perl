bb67f5ab75efb2467e3f4aa2371b100bbb491266
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-03-01 06:27:04 +0800

    2018 Updates

--- Conversions/problem11.pl
+++ Conversions/problem11.pl
@@ -1,5 +1,6 @@
 #Problem 11: Write a program that converts numbers of day to minutes.
-$word1 = "More";
-$word2 = "Codes";
-$newWord = $word1.$word2;
-print($newWord);
+$day = 1;
+$minutes = $day * 24 * 60;
+
+print($minutes);
+
