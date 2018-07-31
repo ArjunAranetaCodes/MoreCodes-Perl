dffa5cef2f37f20b55f48b3dcb0e0737108e7967
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-07-31 05:00:00 +0100

    2018 Updates

---
+++ Regular Expressions/problem20.pl
@@ -0,0 +1,3 @@
+#Problem 20: Write a program that validates if string length is between 5 to 10 using Regular Expression.
+print 'Hello' =~ /\w{5,10}\b/ ? "true\n" : "false\n";
+print 'Hi' =~ /\w{5,10}\b/ ? "true\n" : "false\n";
